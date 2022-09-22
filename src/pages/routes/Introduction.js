841e71beda86d35fbf4b30455e4e164d31f48200
Chen Liang Zhen <felix961308@gmail.com>
2022-09-22 23:53:26 +0800

    change branding size and footer arrangement

---
+++ src/pages/routes/Introduction.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {BaseGrid} from "../components/layouts/Layouts";
+import {PageBody, PageFooter, PageHeader} from "../components/layouts/PageSections";
+
+const IntroductionPage = () => {
+
+	 return (
+		 <BaseGrid>
+
+			 <PageHeader/>
+
+			 <PageBody>
+
+
+
+			 </PageBody>
+
+			 <PageFooter/>
+
+		 </BaseGrid>
+	 )
+}
+
+export default IntroductionPage
+
